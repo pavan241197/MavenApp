c9ab95f5ab0b85a7d22910ee76c837b9d245b6ca
pavan241197 <samipavan@gmail.com>
2020-10-01 14:46:42 +0530

    Initial Commit

---
+++ MavenApp/src/main/java/com/cg/MavenApp/App.java
@@ -0,0 +1,9 @@
+package com.cg.MavenApp;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ MavenApp/src/test/java/com/cg/MavenApp/AppTest.java
@@ -0,0 +1,30 @@
+package com.cg.MavenApp;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+	@Test
+	public void test1() {
+		System.out.println("test sucess");
+
+	}
+
+	@Test
+	public void test2() {
+		System.out.println("test sucess");
+
+	}
+
+	@Test
+	public void test3() {
+		System.out.println("test sucess");
+
+	}
+
+	@Test
+	public void test4() {
+		System.out.println("test sucess");
+
+	}
+
+}
